9eac18e07bbf6d150deec3dcefb0bb529980f78a
FilippiPexe <73662491+FilippiPexe@users.noreply.github.com>
2022-02-11 17:15:07 -0300

    Responsividade
    Criado o menu na responsividade.

--- site/script.js
+++ site/script.js
@@ -1,3 +1,28 @@
+/*BOTÃO*/
+
+const btnMobile = document.getElementById('btn-mobile');
+
+        function toggleMenu(event) {
+            if (event.type === 'touchstart') event.preventDefault();
+            const links = document.getElementById('links');
+            links.classList.toggle('active');
+            const active = links.classList.contains('active');
+            event.currentTarget.setAttribute('aria-expanded', active);
+            if (active) {
+                 event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+            } else {
+                event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+            } 
+        }
+
+
+     btnMobile.addEventListener('click', toggleMenu);
+     btnMobile.addEventListener('touchstart', toggleMenu);
+
+
+
+/************************************************************************************/
+
 let time = 4000,
     currentImageIndex = 0,
     images = document
@@ -26,4 +51,5 @@ function start() { /*lógica de troca de imagem*/
 }
 
 window.addEventListener("load", start)
+
 
